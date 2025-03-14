cde1fc56bc5975fe860073e2d099798206e35476
havreman <93663377+havreman@users.noreply.github.com>
2025-03-14 13:44:24 +0100

    komponenter

---
+++ src/components/ArticleText.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ArticleText = () => (
+    <div className="article-info">
+        <h1>Välkommen på barnvagnsbio</h1>
+        <h3>Bli en del av bio-gemenskapen</h3>
+        <p>
+        Vi visar kvalitetsfilm för spädbarnsföräldrar som längtar efter att få gå på bio!
+        Bebisar under 1 år får följa med in i salongen där nya filmer visas med dämpat ljud i svag belysning.
+        Skötbädd finns tillgänglig. Visningarna äger rum på torsdagar 10:00, biljettpris är 105 kr.
+        Anmäl dig genom att fylla i din e-post nedan.
+        </p>
+    </div>
+  );
+  
+  export default ArticleText;
--- src/pages/Barnvagnsbio.jsx
+++ src/pages/Barnvagnsbio.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "../styles/Artiklar.css";
 import ArticleImage from "../components/ArticleImage";
+import ArticleText from "../components/ArticleText";
+import Button from "../components/Button"
 
 
 class Barnvagnsbio extends React.Component{
@@ -12,15 +14,11 @@ class Barnvagnsbio extends React.Component{
             src="/images/barnvagn.png"
             alt = "En bild från filmen Insidan ut 2"
             className= "main-image"/>
-                <div className="article-info">
-                    <h1>Välkommen på barnvagnsbio</h1>
-                    <h3>Bli en del av bio-gemenskapen</h3>
-                    <p>Vi visar kvalitetsfilm för spädbarnsföräldrar som längtar efter att få gå på bio! Bebisar under 1 år får följa med in i salongen där nya filmer visas med dämpat ljud i svag belysning. Skötbädd finns tillgänglig. Visningarna äger rum på torsdagar 10:00, biljettpris är 105 kr. Anmäl dig genom att fylla i din e-post nedan.</p>
-                    <form action="/action_page.php">
+                <ArticleText/>
+                    <form action="/action_page.php" className="article-info">
                         <label for="email">E-post:</label><br/>
                         <input type="text" id="email" name="email" placeholder="exempel@hotmail.com" />
-                        <button type="submit">Anmäl mig!</button></form>
-                </div>
+                        <Button text="Anmäl mig!" /> </form>
             </main>
         </div>);
         }
