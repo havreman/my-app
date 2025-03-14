8c4a3171e429ddb6d4c1a082d33df934755e0151
havreman <93663377+havreman@users.noreply.github.com>
2025-03-14 15:08:57 +0100

    bookedMovie

--- src/components/CalendarRow.jsx
+++ src/components/CalendarRow.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import Button from "../components/Button";
 
 const CalendarRow = ({ date, salon }) => {
+  const handleBooking = () =>{
+    localStorage.setItem("bookedMovie", `${date} - ${salon}`);
+    alert(`Du har nu bokat: ${date} - ${salon}`);
+  };
   return (
     <tr>
       <td>{date}</td>
       <td>{salon}</td>
       <td>
-        <Button text= "Boka" className="boka-button" />
+        <Button text= "Boka" className="boka-button" onClick={handleBooking}/>
       </td>
     </tr>
   );
